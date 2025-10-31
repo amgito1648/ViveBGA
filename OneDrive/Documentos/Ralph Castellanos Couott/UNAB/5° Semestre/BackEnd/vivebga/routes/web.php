55a8810799ae77694a90bdc1f8c85ef0fbd31894
Ralph Castellanos <rcastellanos168@unab.edu.co>
2025-10-30 19:43:45 -0500

    Inicio de sesión, registro, lenguaje, etc.

--- OneDrive/Documentos/Ralph Castellanos Couott/UNAB/5° Semestre/BackEnd/vivebga/routes/web.php
+++ OneDrive/Documentos/Ralph Castellanos Couott/UNAB/5° Semestre/BackEnd/vivebga/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', [HomeController::class, 'welcome'])->name('welcome');
+
+Auth::routes();
+
+// Si alguien entra a /home, lo redirigimos al inicio:
+Route::get('/home', function () {
+    return redirect()->route('welcome');
 });
